a67a10a3ad51b1d9b37dfcca0411df91ff5dcb39
jlmoldan <jlmoldan@stthomas.edu>
2017-11-03 18:23:14 -0500

    Update naginstall.sh

--- naginstall.sh
+++ naginstall.sh
@@ -30,25 +30,7 @@ chk_user
                                 apt-get update
                                 apt-get upgrade -y
                                 apt-get -y  install apache2 build-essential wget perl openssl 
-                                apt-get -y  install nagios-plugins nagios3 nagios-plugins-basic nagios-plugins-extra nagios-snmp-plugins nagios-nrpe-plugin nagios3-core nagios-plugins-standard nagios3-cgi nagios-plugins-contrib
-                                apt-get -y  install libssl-dev openssh-server openssh-client ntpdate snmp smbclient libldap-2.4-2 libldap2-dev  unzip
-                                sleep 2
-
-                                sed -i 's/check_external_commands=0/check_external_commands=1/g' /etc/nagios3/nagios.cfg
-
-                                /usr/sbin/usermod -a -G www-data nagios
-                                chmod -R g+wx /var/lib/nagios3/
-				##jlmadd/fix apache
-				#mkdir /etc/apache2/conf.d 
-				## taken from http://xubuntugeek.blogspot.com/2012/07/solved-apache2-could-not-reliably.html
-				#echo "ServerName localhost" > /etc/apache2/conf.d/name
-
-                                /etc/init.d/nagios3 restart
-
-                                /etc/init.d/apache2 restart
-                                sleep 2
-                                IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-                                echo -e "\n\tNow Nagios is ready to be used via: http://$IP/nagios3"
+                               
                                 exit 0
 
 
